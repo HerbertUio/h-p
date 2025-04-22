ba22575e697005cd0819b198670f44bb1a6ff0de
Herbert Daniel <herbert.bustamante@uab.edu.bo>
2025-04-22 10:15:45 -0400

    Agregado

---
+++ Help.Desk.Infrastructure/Database/EntityFramework/UseCases/UserUseCases/CreateUserUseCase.cs
@@ -0,0 +1,10 @@
+using Help.Desk.Infrastructure.Database.EntityFramework.Context;
+using Help.Desk.Infrastructure.Database.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Help.Desk.Infrastructure.Database.EntityFramework.UseCases.UserUseCases;
+
+public class CreateUserUseCase
+{
+    
+}
